c42cf1157747b403e33a8a13cefeff40fac200cf
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-26 22:58:53 +0100

    fddfdf

---
+++ ui/buttons/MainBtn.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import clsx from "clsx";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+}
+
+export function MainBtn({ children, className, ...rest }: ButtonProps) {
+    return (
+        <button
+            {...rest}
+            className={clsx(
+                "relative rounded-lg font-semibold text-xs text-white hover:text-zinc-300 focus-visible:outline active:bg-red-50 px-4 py-2 overflow-hidden",
+                className
+            )}
+        >
+            {children}
+            <span className="bg-black z-[-3] absolute h-full w-full rounded-lg left-0 top-0" />
+            <span className="absolute w-full h-2/4 bottom-0 left-0 bg-amber-200 blur-md z-[-2] animate-pulse" />
+        </button>
+    );
+}
