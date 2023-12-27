38497ded3613493631ada6ce261bb576a2651c90
Valkon <96625039+Nawija@users.noreply.github.com>
2023-12-27 17:44:02 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,8 @@ export default function Home() {
               <Image
                 src={i.img}
                 alt="..."
-                height={500}
-                width={500}
+                height={300}
+                width={200}
                 className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-black/10" />
